0901652cd0ee886fb0e4a6d34f2cf78acf2c0f11
tedybak <tedybak@hotmail.com>
2020-09-01 11:20:06 +0100

    express settings, cors, morgan, routes..

--- server/build/index.js
+++ server/build/index.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 const gamesRoutes_1 = __importDefault(require("./routes/gamesRoutes"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 class Server {
     constructor() {
         this.app = express_1.default();
@@ -14,6 +16,10 @@ class Server {
     }
     config() {
         this.app.set("port", process.env.port || 3000);
+        this.app.use(morgan_1.default("dev"));
+        this.app.use(cors_1.default);
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
     }
     routes() {
         this.app.use("/api/v1/", indexRoutes_1.default);
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,9 @@ import express, { Application } from "express";
 import indexRoutes from "./routes/indexRoutes";
 import gamesRoutes from "./routes/gamesRoutes";
 
+import morgan from "morgan";
+import cors from "cors";
+
 class Server {
   public app: Application;
 
@@ -12,6 +15,10 @@ class Server {
   }
   config(): void {
     this.app.set("port", process.env.port || 3000);
+    this.app.use(morgan("dev"));
+    this.app.use(cors);
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
   }
 
   routes(): void {
