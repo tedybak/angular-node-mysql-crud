f5d6e495387f809445b385131d3a1706e2f18842
teddybak <teddybak@hotmail.com>
2020-09-03 13:30:20 +0100

    in progress

--- server/build/index.js
+++ server/build/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 const gamesRoutes_1 = __importDefault(require("./routes/gamesRoutes"));
 const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 class Server {
     constructor() {
         this.app = express_1.default();
@@ -16,7 +17,7 @@ class Server {
     config() {
         this.app.set("port", process.env.port || 3000);
         this.app.use(morgan_1.default("dev"));
-        //this.app.use(cors);
+        this.app.use(cors_1.default());
         this.app.use(express_1.default.json());
         this.app.use(express_1.default.urlencoded({ extended: false }));
     }
--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ class Server {
   config(): void {
     this.app.set("port", process.env.port || 3000);
     this.app.use(morgan("dev"));
-    //this.app.use(cors);
+    this.app.use(cors());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
   }
